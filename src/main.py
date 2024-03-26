16803ff125c6128c3e8d17eb39209eb43a90cace
Andrii Zabolotin <fenics7511@gmail.com>
2024-03-26 14:09:59 +0200

    Add auth routers

--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
+from fastapi_users import FastAPIUsers
 from fastapi import FastAPI
+
+from src.auth.auth import auth_backend
+from src.auth.manager import get_user_manager
+from src.auth.schemas import UserRead, UserCreate, UserUpdate
+from src.models import User
 
 app = FastAPI(
     title="Money Managing App"
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
 )
 
 
