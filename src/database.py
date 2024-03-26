14e2d3c1fab9ced26d619f0a96058b87b50bd5d5
Andrii Zabolotin <fenics7511@gmail.com>
2024-03-26 13:41:02 +0200

    Add get_async_session and get_user_db methods

--- src/database.py
+++ src/database.py
@@ -1,11 +1,25 @@
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from typing import AsyncGenerator
+
 from src.config import settings
+from src.models import User
 
 # Creating a connection to the database
 engine = create_async_engine(settings.db_url)
 
 # Creating a session factory for interacting with the database
 SessionLocal = sessionmaker(engine, class_=AsyncSession, autocommit=False, autoflush=False, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
