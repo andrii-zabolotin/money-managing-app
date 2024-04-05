429d8ca86dfc706941cc1df68917cb6d9d48d869
Andrii Zabolotin <fenics7511@gmail.com>
2024-04-05 13:02:43 +0300

    Add min_length to password

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,4 +1,5 @@
 from fastapi_users import schemas
+from pydantic import Field
 
 
 class UserRead(schemas.BaseUser[int]):
@@ -7,6 +8,7 @@ class UserRead(schemas.BaseUser[int]):
 
 class UserCreate(schemas.BaseUserCreate):
     username: str
+    password: str = Field(min_length=5)
 
 
 class UserUpdate(schemas.BaseUserUpdate):
