e5aa60e9eb8647e32055ffa496f7030fc938c875
Andrii Zabolotin <fenics7511@gmail.com>
2024-04-06 14:12:48 +0300

    Add current_superuser variable

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -25,4 +25,5 @@ fastapi_users = FastAPIUsers[User, int](
     [auth_backend],
 )
 
-current_user = fastapi_users.current_user()
+current_user = fastapi_users.current_user(active=True)
+current_superuser = fastapi_users.current_user(active=True, superuser=True)
