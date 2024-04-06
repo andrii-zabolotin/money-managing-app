34bcc5cd9893a0d2e854851c17a4408fe6b509e9
Andrii Zabolotin <fenics7511@gmail.com>
2024-04-06 09:03:41 +0300

    Create main tables

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, declared_attr
 
 
@@ -14,3 +15,34 @@ class Base(DeclarativeBase):
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     username: Mapped[str] = mapped_column(nullable=False)
+
+
+class Currency(Base):
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+
+class Account(Base):
+    name: Mapped[str] = mapped_column(nullable=False)
+    note: Mapped[str]
+    summ: Mapped[float] = mapped_column(default=0)
+    is_savings_account: Mapped[bool] = mapped_column(default=0)
+    image_url: Mapped[str]
+    fk_currency_id: Mapped[int] = mapped_column(ForeignKey('currency.id'), nullable=False)
+    fk_user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+
+class SavingsAccount(Base):
+    target: Mapped[float] = mapped_column(nullable=False)
+    fk_account_id: Mapped[int] = mapped_column(ForeignKey('account.id'), nullable=False)
+
+
+class Category(Base):
+    name: Mapped[str] = mapped_column(nullable=False, unique=False)
+    image_url: Mapped[str]
+    fk_currency_id: Mapped[int] = mapped_column(ForeignKey('currency.id'), nullable=False)
+    fk_user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+
+class SubCategory(Base):
+    name: Mapped[str] = mapped_column(nullable=False, unique=False)
+    fk_category_id: Mapped[int] = mapped_column(ForeignKey('category.id'), nullable=False)
