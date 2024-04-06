6a4b54191df17faf19f800bc118d5a24dddf2205
Andrii Zabolotin <fenics7511@gmail.com>
2024-04-06 09:01:42 +0300

    Create account schemas

--- src/app/schemas.py
+++ src/app/schemas.py
@@ -3,3 +3,30 @@ from pydantic import BaseModel
 
 class CurrencyRead(BaseModel):
     name: str
+
+
+class AccountRead(BaseModel):
+    name: str
+    note: str
+    summ: float
+    is_savings_account: bool
+    image_url: str
+    fk_currency_id: int
+    fk_user_id: int
+
+
+class AccountCreate(BaseModel):
+    name: str
+    note: str
+    summ: float
+    is_savings_account: bool
+    image_url: str
+    fk_currency_id: int
+
+
+class AccountUpdate(BaseModel):
+    name: str
+    note: str
+    summ: float
+    image_url: str
+    fk_currency_id: int
